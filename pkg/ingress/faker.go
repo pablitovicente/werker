938fbac94b5e61fddb4676414eb7f9f1db9ab06a
Pablo Vicente <pvicente@gmail.com>
2021-09-30 22:13:15 +0200

    Remove uneeded file from pkg process

--- pkg/ingress/faker.go
+++ pkg/ingress/faker.go
@@ -1,14 +1,28 @@
 package ingress
 
 import (
+	"math/rand"
+
 	"github.com/pablitovicente/wrkpool/pkg/models"
-	"github.com/pablitovicente/wrkpool/pkg/process"
 )
 
 func SeedSampleData(numJobs int, jobs chan<- models.Job) {
 	for j := 1; j <= numJobs; j++ {
-		newJob := process.GenerateJob(j)
+		newJob := generateJob(j)
 		jobs <- newJob
 	}
 	close(jobs)
+}
+
+func generateJob(ID int) models.Job {
+	min := 10
+	max := 48
+	randPayload := uint(rand.Intn(max-min) + min)
+
+	job := models.Job{
+		ID:      ID,
+		Payload: randPayload,
+	}
+
+	return job
 }
